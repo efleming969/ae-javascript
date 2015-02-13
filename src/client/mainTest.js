184c8f3c3c72b3e462c8023f1480b5e09fecd9c9
Erick Fleming <erick@sudogs.com>
2015-02-12 21:20:13 -0600

    fix namespace reference for greeting test

--- src/client/mainTest.js
+++ src/client/mainTest.js
@@ -3,7 +3,7 @@ var expect = chai.expect;
 describe ( 'sanity', function () {
 
   it ( 'should succeed', function () {
-    var greeting = new Greeting()
+    var greeting = new app.Greeting()
     var rst = greeting.build("world")
     expect(rst).to.equal("whatup, world")
   })
