d583ae210df1b58b4219b28d51d82a6d76e055fb
Erick Fleming <erick@sudogs.com>
2015-04-15 07:26:02 -0400

    update sanityTest with selenium example

--- test/sanityTest.js
+++ test/sanityTest.js
@@ -1,9 +1,34 @@
 var assert = require( "chai" ).assert
+var webdriverjs = require ( "webdriverio" )
 
 describe( "sanity test", function () {
 
-  it( "sanity", function () {
-    assert.equal( "foo" , "foo" )
+  var browser
+
+  this.timeout(999999);
+
+  beforeEach( function ( done ) {
+
+    browser = webdriverjs.remote (
+      { desiredCapabilities: { browserName: "firefox" }
+      , logLevel: 'info'
+      } )
+
+    browser.init( done )
+
+  } )
+
+  afterEach( function ( done ) {
+    browser.end( done )
+  } )
+
+  it( "sanity", function ( done ) {
+    browser
+      .url( "http://localhost:8080" )
+      .getTitle( function ( err, title ) {
+        assert.equal ( title, "foo" )
+      } )
+      .call( done )
   })
 
 })
