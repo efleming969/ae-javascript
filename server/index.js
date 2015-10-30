a24055f8b044687d80229aa191feab78760cc5ef
Erick Fleming <erick@sudogs.com>
2015-10-30 06:20:29 -0500

    remove api sample from server code

--- server/index.js
+++ server/index.js
@@ -6,10 +6,6 @@ var express = require( "express" );
 
 var app = express();
 
-app.get( "/api/hello", function( req, res ) {
-  res.send( { "message":"hello" } );
-} );
-
 app.use( bodyParser.json() );
 app.use( express.static( path.resolve( "client" ) ) );
 app.use( "/dependencies", express.static( path.resolve( "node_modules" ) ) );
